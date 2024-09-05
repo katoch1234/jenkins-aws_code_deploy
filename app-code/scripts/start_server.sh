080ce2f3964668328f29f33d0342c5908926b40f
cu.16bcs1092 <cu.16bcs1092@gmail.com>
2024-09-05 14:16:27 +0530

    added sed command to comment out sites-enabled in nginx.conf

--- app-code/scripts/start_server.sh
+++ app-code/scripts/start_server.sh
@@ -10,4 +10,5 @@ sudo npm install
 sudo nohup node index.js 2>&1 | sudo tee /var/log/app.log > /dev/null &
 
 #restart nginx
+sudo sed -i '/include \/etc\/nginx\/sites-enabled\/\*;/s/^/#/' /etc/nginx/nginx.conf
 sudo systemctl restart nginx
